4a04b4fced4879b5472aca108aa65288e518685e
ChrisGeorgeDev <72430985+ChrisGeorgeDev@users.noreply.github.com>
2022-06-21 09:38:44 -0400

    wip

--- components/MainNav/OverlayMenu.js
+++ components/MainNav/OverlayMenu.js
@@ -55,8 +55,8 @@ export default function OverlayMenu({ open, setOpen, title, children }) {
                   >
                     <span className="sr-only">Close panel</span>
                     {/* <XIcon className=" text-orange h-16 w-16" aria-hidden="true" /> */}
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
-  <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 20 20" fill="#000">
+  <path fillRule="evenodd" fill='#000' d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
 </svg>
        
                   </button>
